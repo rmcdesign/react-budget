ba4b78135cdeeaec025b8f1efaa180f6bf51689e
rmcdesign <info@rmc-design.co.uk>
2017-03-21 22:57:34 +0000

    Addded income and expense buttons

--- app/components/BudgetApp.jsx
+++ app/components/BudgetApp.jsx
@@ -19,15 +19,26 @@ var BudgetApp = React.createClass({
     return (
       <div>
         <Nav/>
-            <div className="page-container test">
-                <ReactCSSTransitionGroup {...transitionOptions}>
-                {
-                  React.cloneElement(this.props.children, {
-                      key: this.props.location.key
-                  })
-                }
-                </ReactCSSTransitionGroup>
-            </div>
+        <div className="page-container test">
+            <ReactCSSTransitionGroup {...transitionOptions}>
+            {
+              React.cloneElement(this.props.children, {
+                  key: this.props.location.key
+              })
+            }
+            </ReactCSSTransitionGroup>
+        </div>
+          <div className="buttons-container">
+              <div className="tracker-input-container">
+                  <input type="number" className="tracker-input" placeholder="Add Income or Expense here"/>
+              </div>
+
+              <div className="buttons">
+                  <button>Income</button>
+                  <button>Expense</button>
+              </div>
+          </div>
+
       </div>
     )
   }
